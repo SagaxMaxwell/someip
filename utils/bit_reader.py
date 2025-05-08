8f48b04490e1c869a97e15b3f2a9d968964b24bb
SagaxMaxwell <sagax.maxwell@gmail.com>
2025-05-08 23:59:57 +0800

    Upddated BitReader class

--- utils/bit_reader.py
+++ utils/bit_reader.py
@@ -1,12 +1,29 @@
 __all__ = ["BitReader"]
 
 
-class BitReader:
-    def __init__(self, bits):
-        self.bits = bits
-        self.index = 0
+from bitarray import bitarray
 
-    def read(self, length):
+
+class BitReader:
+    def __init__(self, bits: bitarray) -> None:
+        self.__bits = bits
+        self.__index = 0
+
+    @property
+    def bits(self) -> bitarray:
+        return self.__bits
+
+    @property
+    def index(self) -> int:
+        return self.__index
+
+    @index.setter
+    def index(self, value: int) -> None:
+        if value < 0 or value > len(self.bits):
+            raise ValueError("Index out of range")
+        self.__index = value
+
+    def read(self, length: int) -> bitarray:
         value = self.bits[self.index : self.index + length]
         self.index += length
         return value
